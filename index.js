3ba0e35c69dedc372d48bb83084a30f7a2f80d17
pankajmehra332 <pankaj@codebuddy.co>
2025-08-22 15:15:16 +0530

    add more functions

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/login', (req, res) => {
+  res.send('Please login!')
+})
+
+app.get('/twitter', (req, res) => {
+  res.send('This is your twitter username!')
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
 })
